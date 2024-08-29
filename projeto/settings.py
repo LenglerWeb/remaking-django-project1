fcaa79d1ba3b01aa1e2060687323c1ad1b4ef4fe
Eduardo Lengler <lenglerweb@gmail.com>
2024-08-29 10:02:57 -0300

    Finalizada aula 46

--- projeto/settings.py
+++ projeto/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'base_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'base_static',
+    
 ]
 
 STATIC_ROOT = BASE_DIR / 'static'
