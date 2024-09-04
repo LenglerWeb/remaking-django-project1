62c5dd0f60a860d525750a78bf3ef044556c8a87
Eduardo Lengler <lenglerweb@gmail.com>
2024-09-04 11:39:13 -0300

    Finalizada aula 49 e iniciado a aula 51

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
